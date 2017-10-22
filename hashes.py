b18713e34f9eb70b9234e81ade91ef04484d4d2d
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-22 23:06:12 +0200

    Trying hashes

---
+++ hashes.py
@@ -0,0 +1,32 @@
+days = {
+  'Sunday':
+    '''Sunday
+    What a great day
+    Ends the weekend
+    wait I'm actually sad because weekend is over''',
+  'Monday':
+    '''Difficult day
+    Hard to work
+    Headaches
+    Omg''',
+  'Tuesday':
+    '''Tuesday is after Monday
+    Second day of the weekend
+    If the week starts monday for you''',
+  'Wednesday':
+    '''Wednesday
+    What a great day
+    ''',
+  'Thursday':
+    '''Difficult day
+    Before friday''',
+  'Friday':
+    '''Friday is last day of the weekend
+    Party''',
+  'Saturday':
+    '''Saturday what
+    should i Do'''
+}
+
+print '='*50
+print 'The start of the week, wait which day is that?', days['Monday']
