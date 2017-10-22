ea0fb1b65fe3f4c88fe73fbf8b254c9f8b1b3593
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-22 22:42:19 +0200

    String formatting

--- playground.py
+++ playground.py
@@ -1,4 +1,14 @@
-print(2 + 1)
-print(2 * 4)
-print(7 * 5)
-print(20 / 10.0)
+# print(2 + 1)
+# print(2 * 4)
+# print(7 * 5)
+# print(20 / 10.0)
+#
+# numberOne = 5
+# numberTwo = 10
+#
+# print(numberOne + numberTwo)
+
+greeting = 'Hello there'
+name = 'John Doe'
+
+print(greeting + ' ' + name)
---
+++ stringformatting.py
@@ -0,0 +1,7 @@
+print('''How many days are in each month::
+January       -{2}
+February      -{0}
+March         -{2}
+April         -{1}
+May           -{2}
+June          -{1}'''.format(28,30,31))
