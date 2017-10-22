f74dafcb9f485068ae2c42e927d3ab4dc775ec94
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-22 22:54:24 +0200

    Testing arrays

---
+++ arrays.py
@@ -0,0 +1,17 @@
+newArray = ["mashrur", "steve", "StackSkills"]
+print(newArray)
+
+newArray[0] = 'blabla'
+print(newArray)
+
+newArray[1] = 'Rita'
+print(newArray)
+
+newArray.append('UK')
+print(newArray)
+
+newArray.extend(['scotland', 'Ireland', 'Norway'])
+print(newArray)
+
+newArray.remove('scotland')
+print(newArray)
